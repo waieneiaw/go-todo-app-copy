9530eb6bbcb70b0fb452e59e51074cb6ee39db3c
waieneiaw <15177530+waieneiaw@users.noreply.github.com>
2023-08-12 00:31:35 +0000

    refactor: start server in separate goroutine

--- main.go
+++ main.go
@@ -5,16 +5,40 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+
+	"golang.org/x/sync/errgroup"
 )
 
 func run(ctx context.Context) error {
 	s := &http.Server{
 		Addr: ":18080",
 		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			fmt.Fprintf(w, "Hello %s!", r.URL.Path[1:])
+			fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
 		}),
 	}
-	return s.ListenAndServe()
+
+	eg, ctx := errgroup.WithContext(ctx)
+
+	// 別ゴルーチンでHTTPサーバーを起動する
+	eg.Go(func() error {
+		if err := s.ListenAndServe(); err != nil &&
+			// Http.ErrServerClosedはシャットダウンが正常終了したことを示すため除外する
+			err != http.ErrServerClosed {
+			log.Printf("failed to close: %+v", err)
+			return err
+		}
+
+		return nil
+	})
+
+	<-ctx.Done() // チャネルからの通知を待機する
+
+	if err := s.Shutdown(context.Background()); err != nil {
+		log.Printf("failed to shutdown: %+v", err)
+	}
+
+	// 別ゴルーチンの終了を待つ
+	return eg.Wait()
 }
 
 func main() {
