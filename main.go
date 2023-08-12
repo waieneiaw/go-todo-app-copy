fe07ff80ccb8c6c586a9e13e2cec2468a6ce9a08
waieneiaw <15177530+waieneiaw@users.noreply.github.com>
2023-08-12 00:02:10 +0000

    refactor: split `main` into `run` func

--- main.go
+++ main.go
@@ -1,20 +1,24 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"log"
 	"net/http"
-	"os"
 )
 
-func main() {
-	err := http.ListenAndServe(
-		":18080",
-		http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+func run(ctx context.Context) error {
+	s := &http.Server{
+		Addr: ":18080",
+		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			fmt.Fprintf(w, "Hello %s!", r.URL.Path[1:])
 		}),
-	)
-	if err != nil {
-		fmt.Printf("failed to terminate server: %v", err)
-		os.Exit(1)
+	}
+	return s.ListenAndServe()
+}
+
+func main() {
+	if err := run(context.Background()); err != nil {
+		log.Printf("failed to terminate server: %v", err)
 	}
 }
