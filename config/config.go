86c57a741bed6150eb33cc0ed220e8586fb20b74
waieneiaw <15177530+waieneiaw@users.noreply.github.com>
2023-08-19 13:19:08 +0000

    feat: add configs from env vars

---
+++ config/config.go
@@ -0,0 +1,17 @@
+package config
+
+import "github.com/caarlos0/env/v9"
+
+type Config struct {
+	Env  string `env:"TODO_ENV" envDefault:"dev"`
+	Port int    `env:"PORT" envDefault:"80"`
+}
+
+func New() (*Config, error) {
+	cfg := &Config{}
+	if err := env.Parse(cfg); err != nil {
+		return nil, err
+	}
+
+	return cfg, nil
+}
