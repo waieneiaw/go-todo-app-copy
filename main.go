1aee81b2bc58e6227cebee750c74943fe3b49a3c
waieneiaw <15177530+waieneiaw@users.noreply.github.com>
2023-08-19 05:48:40 +0000

    feat: listen on a specified port

--- main.go
+++ main.go
@@ -4,14 +4,15 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"net"
 	"net/http"
+	"os"
 
 	"golang.org/x/sync/errgroup"
 )
 
-func run(ctx context.Context) error {
+func run(ctx context.Context, l net.Listener) error {
 	s := &http.Server{
-		Addr: ":18080",
 		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
 		}),
@@ -21,7 +22,7 @@ func run(ctx context.Context) error {
 
 	// 別ゴルーチンでHTTPサーバーを起動する
 	eg.Go(func() error {
-		if err := s.ListenAndServe(); err != nil &&
+		if err := s.Serve(l); err != nil &&
 			// Http.ErrServerClosedはシャットダウンが正常終了したことを示すため除外する
 			err != http.ErrServerClosed {
 			log.Printf("failed to close: %+v", err)
@@ -42,7 +43,18 @@ func run(ctx context.Context) error {
 }
 
 func main() {
-	if err := run(context.Background()); err != nil {
+	if len(os.Args) != 2 {
+		log.Printf("need port number\n")
+		os.Exit(1)
+	}
+
+	p := os.Args[1]
+	l, err := net.Listen("tcp", ":"+p)
+	if err != nil {
+		log.Fatalf("failed to listen port %s: %v", p, err)
+	}
+	if err := run(context.Background(), l); err != nil {
 		log.Printf("failed to terminate server: %v", err)
+		os.Exit(1)
 	}
 }
