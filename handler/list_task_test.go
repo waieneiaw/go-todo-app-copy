6cdfc1db5fbd01229177ee480acaaff64c81312e
waieneiaw <15177530+waieneiaw@users.noreply.github.com>
2023-08-21 23:37:20 +0000

    feat: add `ListTask` handler

---
+++ handler/list_task_test.go
@@ -0,0 +1,80 @@
+package handler
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/waieneiaw/go-todo-app-copy/entity"
+	"github.com/waieneiaw/go-todo-app-copy/store"
+	"github.com/waieneiaw/go-todo-app-copy/testutil"
+)
+
+func TestListTask(t *testing.T) {
+	type want struct {
+		status  int
+		rspFile string
+	}
+	tests := map[string]struct {
+		tasks []*entity.Task
+		want  want
+	}{
+		"ok": {
+			tasks: []*entity.Task{
+				{
+					ID:     1,
+					Title:  "test1",
+					Status: entity.TaskStatusTodo,
+				},
+				{
+					ID:     2,
+					Title:  "test2",
+					Status: entity.TaskStatusDone,
+				},
+			},
+			want: want{
+				status:  http.StatusOK,
+				rspFile: "testdata/list_task/empty_rsp.json.golden",
+			},
+		},
+		"empty": {
+			tasks: []*entity.Task{
+				{
+					ID:     1,
+					Title:  "test1",
+					Status: entity.TaskStatusTodo,
+				},
+				{
+					ID:     2,
+					Title:  "test2",
+					Status: entity.TaskStatusDone,
+				},
+			},
+			want: want{
+				status:  http.StatusOK,
+				rspFile: "testdata/list_task/empty_rsp.json.golden",
+			},
+		},
+	}
+
+	for n, tt := range tests {
+		tt := tt
+		t.Run(n, func(t *testing.T) {
+			t.Parallel()
+
+			w := httptest.NewRecorder()
+			r := httptest.NewRequest(http.MethodGet, "/tasks", nil)
+
+			sut := ListTask{
+				Store: &store.TaskStore{
+					Tasks: map[entity.TaskID]*entity.Task{},
+				},
+			}
+			sut.ServeHTTP(w, r)
+
+			resp := w.Result()
+			testutil.AssertResponse(t, resp, tt.want.status, testutil.LoadFile(t, tt.want.rspFile))
+		})
+	}
+
+}
